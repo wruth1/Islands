5991dae326f7cf4b00e1f9d2c10bd1c76b7f4040
William <wruth@sfu.ca>
2019-10-24 19:49:39 -0700

    Add analysis and report for hill running study

--- Hill_Running_Analysis.R
+++ Hill_Running_Analysis.R
@@ -1,2 +1,18 @@
-data = read.csv("Hill_Running.csv")
+library(tidyverse)
 
+data.raw = read.csv("Hill_Running.csv")
+
+data = mutate(data.raw, Change = After - Before,
+              rel.change = Change/Before)
+
+hist(data$rel.change)
+
+Uphill = (data$Type == "Up")
+
+Up.Change = data[Uphill,"Change"]
+Down.Change = data[!Uphill, "Change"]
+test.Change = t.test(Up.Change, Down.Change)
+
+Up.rel.change = data[Uphill, "rel.change"]
+Down.rel.change = data[!Uphill, "rel.change"]
+test.rel.change = t.test(Up.rel.change, Down.rel.change)
